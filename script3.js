7a1efa4848dec9a043fe9b5846abfeeaaf0a90ad
sergsem72 <49842180+sergsem72@users.noreply.github.com>
2023-05-11 23:43:25 +0300

    Update script3.js

--- script3.js
+++ script3.js
@@ -25,9 +25,9 @@ var blue_break = new Audio();
 var samolet_fall = new Audio();
 fly.src = "https://raw.githubusercontent.com/sergsem72/avia5/main/fly.mp3";
 fly.loop = true;
-red_break.src = "https://github.com/sergsem72/avia5/blob/main/red_break2.mp3";
-blue_break.src = "https://github.com/sergsem72/avia5/blob/main/blue_break.mp3";
-samolet_fall.src = "https://github.com/sergsem72/avia5/blob/main/samolet.mp3";
+red_break.src = "https://raw.githubusercontent.com/sergsem72/avia5/main/red_break2.mp3";
+blue_break.src = "https://raw.githubusercontent.com/sergsem72/avia5/main/blue_break.mp3";
+samolet_fall.src = "https://raw.githubusercontent.com/sergsem72/avia5/main/samolet.mp3";
 
 function resetGame() {
   game = {
